9c29c3281d36bcfecba723585e7c5dc91c9b7bd6
erasme <erasme@labmonitoring.erasme.lan>
2016-07-08 10:45:20 +0200

    Minor Bug

--- index.js
+++ index.js
@@ -8,21 +8,15 @@ module.exports = function (sails) {
 		policies : __dirname + '/policies',
 		config : __dirname + '/config'
 	});
-	//var install = require('./lib/install')(sails);
-	gladys.on('ready', function(){
-		var install = require('./lib/install');
-		install();
-	});
-
 	return {
 
-       	    //defaults: require('./lib/defaults'),
-	    //configure: require('./lib/configure')(sails),
-	    //initialize: require('./lib/initialize')(sails),
+       	    defaults: require('./lib/defaults'),
+	    configure: require('./lib/configure')(sails),
+	    initialize: require('./lib/initialize')(sails),
 	    //exec: require('./lib/exec')(sails),
 	    //setup: require('./lib/setup')(sails),
             uninstall: require('./lib/uninstall')(sails),
-	    //install:require('./lib/install')(sails),
+	    install:require('./lib/install')(sails),
             //routes: require('./lib/routes')(sails),
 
 	};
